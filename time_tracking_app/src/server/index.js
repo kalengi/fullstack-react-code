5bdcac13e4b4e03f57f0538a3fc021d58ef5ae7c
Alex Githatu <kalengi@bitbucket.org>
2020-04-27 15:06:02 +0300

    -- setting up the project structure and code to run on codesandbox with concurrent react client and node server: --- sandbox template: https://codesandbox.io/s/obm62 --- documentation https://github.com/crsandeep/simple-react-full-stack

--- time_tracking_app/src/server/index.js
+++ time_tracking_app/src/server/index.js
@@ -1,33 +1,141 @@
 const express = require("express");
-const os = require("os");
-const fetch = require("node-fetch");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+//const os = require("os");
+//const fetch = require("node-fetch");
 
 const app = express();
 
-app.use(express.static("dist"));
-app.use(express.json());
+const DATA_FILE = path.join(__dirname, "./data/data.json");
 
-app.get("/api/getUsername", (req, res) =>
-  res.send({ username: os.userInfo().username })
-);
+const PORT = 8000;
+
+//app.use(express.static("dist"));
+//let staticFilesPath = path.join(__dirname, "../../public");
+let staticFilesPath = path.join(__dirname, "../client/build");
+app.use("/", express.static(staticFilesPath));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send("Tuko hapa");
+  //debugger;
+
+  let indexHtmlPath = path.join(staticFilesPath, "/index.html");
+  res.sendFile(indexHtmlPath, function(error) {
+    if (error) {
+      //debugger;
+
+      let errorMessage = `The index file could not be served: ${error.message}`;
+      console.log(errorMessage);
+      return res.status(500).json({ message: errorMessage });
+    }
+  });
 });
+
 app.post("/api/check-auth", async (req, res) => {
-  const { login, password } = req.body.user;
-  const url = `http://localhost:3004/users?login=${login}`;
-
-  const data = await fetch(url);
-  const users = await data.json();
-  const [user] = users;
-
-  if (user && user.password === password) {
-    res.json({ user: user.login });
-  } else {
-    res.status(403).end();
-  }
+  res.json({ user: "Admin" });
 });
 
-app.listen(process.env.PORT || 8000, () =>
-  console.log(`Listening on port ${process.env.PORT || 8000}!`)
-);
+app.get("/api/timers", (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    res.setHeader("Cache-Control", "no-cache");
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/timers", (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    const timers = JSON.parse(data);
+    const newTimer = {
+      title: req.body.title,
+      project: req.body.project,
+      id: req.body.id,
+      elapsed: 0,
+      runningSince: null
+    };
+    timers.push(newTimer);
+    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
+      res.setHeader("Cache-Control", "no-cache");
+      res.json(timers);
+    });
+  });
+});
+
+app.post("/api/timers/start", (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    const timers = JSON.parse(data);
+    timers.forEach(timer => {
+      if (timer.id === req.body.id) {
+        timer.runningSince = req.body.start;
+      }
+    });
+    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
+      res.json({});
+    });
+  });
+});
+
+app.post("/api/timers/stop", (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    const timers = JSON.parse(data);
+    timers.forEach(timer => {
+      if (timer.id === req.body.id) {
+        const delta = req.body.stop - timer.runningSince;
+        timer.elapsed += delta;
+        timer.runningSince = null;
+      }
+    });
+    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
+      res.json({});
+    });
+  });
+});
+
+app.put("/api/timers", (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    const timers = JSON.parse(data);
+    timers.forEach(timer => {
+      if (timer.id === req.body.id) {
+        timer.title = req.body.title;
+        timer.project = req.body.project;
+      }
+    });
+    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
+      res.json({});
+    });
+  });
+});
+
+app.delete("/api/timers", (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    let timers = JSON.parse(data);
+    timers = timers.reduce((memo, timer) => {
+      if (timer.id === req.body.id) {
+        return memo;
+      } else {
+        return memo.concat(timer);
+      }
+    }, []);
+    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
+      res.json({});
+    });
+  });
+});
+
+app.get("/molasses", (_, res) => {
+  setTimeout(() => {
+    res.end();
+  }, 5000);
+});
+
+app.listen(PORT, function() {
+  console.log("Server is running on Port: " + PORT);
+});
