ba92b1adab20140e23201d7d1f44d1072172fece
Alex Githatu <kalengi@bitbucket.org>
2020-05-01 16:00:49 +0300

    -- applying the appropriate title for the project -- coding progress pg 187

--- forms/src/index.js
+++ forms/src/index.js
@@ -4,7 +4,7 @@ import "../public/semantic-ui/semantic.min.css";
 import "../public/index.css";
 import Element from "./App";
 
-const rootElement = document.getElementById("content");
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <Element />
