802fb47ae7af009bc51c00df6e9d15f1ef27c908
Alex Githatu <kalengi@bitbucket.org>
2020-05-15 14:07:35 +0300

    -- split the reducer into sub-reducers for better code organization -- coding progress pg 535

--- redux/chat_intermediate/src/App.js
+++ redux/chat_intermediate/src/App.js
@@ -9,38 +9,50 @@ import { v4 as uuidv4 } from 'uuid';
 import { createStore } from 'redux';
 
 function reducer(state, action) {
+  return {
+    activeThreadId: activeThreadIdReducer(state.activeThreadId, action),
+    threads: threadsReducer(state.threads, action),
+  };
+}
+
+function activeThreadIdReducer(state, action) {
+  if (action.type === 'OPEN_THREAD') {
+    return action.id;
+  } else {
+    return state;
+  }
+}
+
+function threadsReducer(state, action) {
   if (action.type === 'ADD_MESSAGE') {
     const newMessage = {
       text: action.text,
       timestamp: Date.now(),
       id: uuidv4(),
     };
-    const threadIndex = state.threads.findIndex(
+    const threadIndex = state.findIndex(
       (t) => t.id === action.threadId
     );
-    const oldThread = state.threads[threadIndex];
+    const oldThread = state[threadIndex];
     const newThread = {
       ...oldThread,
       messages: oldThread.messages.concat(newMessage),
     };
 
-    return {
-      ...state,
-      threads: [
-        ...state.threads.slice(0, threadIndex),
-        newThread,
-        ...state.threads.slice(
-          threadIndex + 1, state.threads.length
-        ),
-      ],
-    };
+    return [
+      ...state.slice(0, threadIndex),
+      newThread,
+      ...state.slice(
+        threadIndex + 1, state.length
+      ),
+    ];
   } else if (action.type === 'DELETE_MESSAGE') {
-    const threadIndex = state.threads.findIndex(
+    const threadIndex = state.findIndex(
       (t) => t.messages.find((m) => (
         m.id === action.id
       ))
     );
-    const oldThread = state.threads[threadIndex];
+    const oldThread = state[threadIndex];
 
     const newThread = {
       ...oldThread,
@@ -49,21 +61,13 @@ function reducer(state, action) {
       )),
     };
 
-    return {
-      ...state,
-      threads: [
-        ...state.threads.slice(0, threadIndex),
-        newThread,
-        ...state.threads.slice(
-          threadIndex + 1, state.threads.length
-        ),
-      ],
-    };
-  } else if (action.type === 'OPEN_THREAD') {
-    return {
-      ...state,
-      activeThreadId: action.id,
-    };
+    return [
+      ...state.slice(0, threadIndex),
+      newThread,
+      ...state.slice(
+        threadIndex + 1, state.length
+      ),
+    ];
   } else {
     return state;
   }
@@ -120,6 +124,8 @@ class App extends React.Component {
     );
   }
 }
+
+/* eslint-disable react/prefer-stateless-function */
 
 class ThreadTabs extends React.Component {
   handleClick = (id) => {
